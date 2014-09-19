6eb19ed8312f18825a7e676617ddf48bbc1e1f24
Fernando Shiro <fernando@42i.com.br>
2014-09-19 20:58:30 -0300

    initial commit

---
+++ js/color-variables.js
@@ -0,0 +1,88 @@
+/* exported colorVariables */
+
+'use strict';
+
+var colorVariables = {
+    //Grays
+    c_white: '#FFFFFF',
+    c_gray_l3: '#f5f5f5',
+    c_gray_l2: '#e5e5e5',
+    c_gray_l1: '#cccccc',
+    c_gray: '#9b9b9b',
+    c_gray_d1: '#818181',
+    c_gray_d2: '#606060',
+    c_gray_d3: '#414141',
+    c_black: '#000000',
+
+
+    // Primary Colors
+    // -------------------------
+
+    //Dark green
+    c_dark_green_l3: '#e1efee',
+    c_dark_green_l2: '#b7d9d6',
+    c_dark_green_l1: '#75b5af',
+    c_dark_green: '#006f65',
+    c_dark_green_d1: '#005d54',
+    c_dark_green_d2: '#004640',
+    c_dark_green_d3: '#00302d',
+
+    //Green
+    c_green_l3: '#e1f8f0',
+    c_green_l2: '#b7edda',
+    c_green_l1: '#75dcb8',
+    c_green: '#00b976',
+    c_green_d1: '#009a63',
+    c_green_d2: '#00734a',
+    c_green_d3: '#004d33',
+
+    //Yellow
+    c_yellow_l3: '#fff5e1',
+    c_yellow_l2: '#ffe7b7',
+    c_yellow_l1: '#ffcf75',
+    c_yellow: '#ffa004',
+    c_yellow_d1: '#d48602',
+    c_yellow_d2: '#9c6402',
+    c_yellow_d3: '#684400',
+
+    //Orange
+    c_orange_l3: '#ffede1',
+    c_orange_l2: '#ffd4b7',
+    c_orange_l1: '#ffaa75',
+    c_orange: '#ff5b02',
+    c_orange_d1: '#d44d02',
+    c_orange_d2: '#9c3a00',
+    c_orange_d3: '#682800',
+
+
+    // Secondary Colors
+    // -------------------------
+
+    //Dark blue
+    c_dark_blue_l2: '#b7d2ed',
+    c_dark_blue_l1: '#74a7da',
+    c_dark_blue: '#0056b7',
+    c_dark_blue_d1: '#004999',
+    c_dark_blue_d2: '#003771',
+
+    //Blue
+    c_blue_l2: '#b7eff3',
+    c_blue_l1: '#75dfe9',
+    c_blue: '#00c0d4',
+    c_blue_d1: '#009fb0',
+    c_blue_d2: '#007782',
+
+    //Magenta
+    c_magenta_l2: '#f2b7d1',
+    c_magenta_l1: '#e775a6',
+    c_magenta: '#ce0054',
+    c_magenta_d1: '#ab0047',
+    c_magenta_d2: '#7f0036',
+
+    //Purple
+    c_purple_l2: '#deb7ce',
+    c_purple_l1: '#bd75a0',
+    c_purple: '#7f004b',
+    c_purple_d1: '#6a003f',
+    c_purple_d2: '#500030'
+};
