5dddbeceb5317e96119e55383c7ee0685061cc3e
Fernando Shiro <fernando@42i.com.br>
2015-06-20 19:16:17 -0300

    fixed js bugs

--- js/main.js
+++ js/main.js
@@ -1,67 +1,87 @@
-//header-resize
-$('.js-page-wrapper').css('padding-top', $('.header').outerHeight());
-$('.js-home-cover').outerHeight($(window).outerHeight() - $('.header').outerHeight() - $('.footer').outerHeight());
-$('.js-project-cover').outerHeight($(window).outerHeight());
-$('.js-project-cover-img').outerHeight($(window).outerHeight());
+var $window= $(window);
 
-$(window).resize(function(){
-  $('.js-page-wrapper').css('padding-top', $('.header').outerHeight());
-  $('.js-home-cover').outerHeight($(window).outerHeight() - $('.header').outerHeight() - $('.footer').outerHeight());
-  $('.js-project-cover').outerHeight($(window).outerHeight());
-  $('.js-project-cover-img').outerHeight($(window).outerHeight());
-});
+function coverResize() {
+    var $header = $('.header');
 
-// Compressed Header
-var isMenuFixed = false;
+    $('.js-page-wrapper').css('padding-top', $header.outerHeight());
+    $('.js-home-cover').outerHeight($window.outerHeight() - $header.outerHeight() - $('.footer').outerHeight());
+    $('.js-project-cover').outerHeight($window.outerHeight());
+    $('.js-project-cover-img').outerHeight($window.outerHeight());
+}
 
-$(window).scroll(function(){
-    var scroll = document.body.scrollTop;
+function onPageScroll() {
+    var isMenuFixed = false;
+    var topDif = 50;
+    var $header = $('.js-header');
+    var $headerCompressed = 'header--compressed';
+    var $cover = $('.js-project-cover');
+    var $coverScrolled = 'cover--scrolled';
+    var $techSpecs = $('.js-tech-specs');
+    var $techSpecsFixed = 'tech-specs--fixed';
 
-    if (scroll < 50 && isMenuFixed) {
-        if ($('.js-header').hasClass('header--compressed')) {
-            $('.js-header').removeClass('header--compressed');
-            $('.js-header__brand').removeClass('header__brand--compressed');
-            $('.js-header__list').removeClass('header__list--compressed');
-            $('.js-header__link').removeClass('header__link--compressed');
-            isMenuFixed = false;
-        }
+    if($techSpecs.length){
+        var $techSpecsOffset = $techSpecs.offset().top - $header.outerHeight();
+    }
 
-        if ($('.js-project-cover').hasClass('cover--scrolled')) {
-            $('.js-project-cover').removeClass('cover--scrolled');
-            isMenuFixed = false;
-        }
-    } else if (scroll > 50 && !isMenuFixed) {
-        if (!$('.js-header').hasClass('header--compressed')) {
-            $('.js-header').addClass('header--compressed');
-            $('.js-header__brand').addClass('header__brand--compressed');
-            $('.js-header__list').addClass('header__list--compressed');
-            $('.js-header__link').addClass('header__link--compressed');
-            isMenuFixed = true;
-        }
+    function headerResize() {
+        var scroll = document.body.scrollTop;
 
-        if (!$('.js-project-cover').hasClass('cover--scrolled')) {
-            $('.js-project-cover').addClass('cover--scrolled');
-            isMenuFixed = true;
+        if (scroll < topDif && isMenuFixed) {
+            if ($header.hasClass($headerCompressed)) {
+                $header.removeClass($headerCompressed);
+                $('.js-header__brand').removeClass('header__brand--compressed');
+                $('.js-header__list').removeClass('header__list--compressed');
+                $('.js-header__link').removeClass('header__link--compressed');
+                isMenuFixed = false;
+            }
+
+            if ($cover.hasClass($coverScrolled)) {
+                $cover.removeClass($coverScrolled);
+                isMenuFixed = false;
+            }
+
+        } else if (scroll > topDif && !isMenuFixed) {
+            if (!$header.hasClass($headerCompressed)) {
+                $header.addClass($headerCompressed);
+                $('.js-header__brand').addClass('header__brand--compressed');
+                $('.js-header__list').addClass('header__list--compressed');
+                $('.js-header__link').addClass('header__link--compressed');
+                isMenuFixed = true;
+            }
+
+            if (!$cover.hasClass($coverScrolled)) {
+                $cover.addClass($coverScrolled);
+                isMenuFixed = true;
+            }
         }
     }
 
-    if (scroll >= $('.js-tech-specs').offset().top - $('.js-header').outerHeight()) {
-        $('.js-tech-specs').addClass('tech-specs--fixed');
-    } else {
-        $('.js-tech-specs').removeClass('tech-specs--fixed');
+    function techSpecsToTop() {
+        var scroll = document.body.scrollTop;
+
+        if (scroll >= $techSpecsOffset) {
+            $techSpecs.addClass($techSpecsFixed);
+        } else {
+            $techSpecs.removeClass($techSpecsFixed);
+        }
     }
-});
 
-// Google Map Styles
-var map;
-var genJardim = new google.maps.LatLng(-23.5446301, -46.6508648);
+    $window.scroll(function(){
+         headerResize();
+         techSpecsToTop();
+    });
+}
 
-var MY_MAPTYPE_ID = 'custom_style';
 
-function initialize() {
+function contactMap(){
+    var map;
+    var genJardim = new google.maps.LatLng(-23.5446301, -46.6508648);
 
-  var featureOpts = [
-   {
+    var MY_MAPTYPE_ID = 'custom_style';
+
+
+    var featureOpts = [
+    {
       featureType: 'landscape',
       elementType: 'all',
       stylers: [
@@ -132,34 +152,41 @@ function initialize() {
         { color: colorVariables.c_blue }
       ]
     }
-  ];
+    ];
 
-  var mapOptions = {
+    var mapOptions = {
     zoom: 15,
     center: genJardim,
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
     },
     mapTypeId: MY_MAPTYPE_ID
-  };
+    };
 
-  map = new google.maps.Map(document.getElementById('js-map-canvas'), mapOptions);
+    map = new google.maps.Map(document.getElementById('js-map-canvas'), mapOptions);
 
-  var styledMapOptions = {
+    var styledMapOptions = {
     name: 'Custom Style'
-  };
+    };
 
-  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
 
-  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+    map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
 
-  var image = '';
-  var marker = new google.maps.Marker({
+    var image = '';
+    var marker = new google.maps.Marker({
       position: genJardim,
       map: map,
       title: 'We are here!',
       icon: image
-  });
+    });
 }
 
-google.maps.event.addDomListener(window, "load", initialize);
+
+coverResize();
+onPageScroll();
+contactMap();
+
+$window.resize(function(){
+  coverResize();
+});
